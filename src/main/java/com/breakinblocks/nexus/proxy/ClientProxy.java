194dbf279053b0588b9074353602284eec8ec702
TeamDman <TeamDman9201@gmail.com>
2017-01-27 19:34:18 -0500

    Fix crash on launch

--- src/main/java/com/breakinblocks/nexus/proxy/ClientProxy.java
+++ src/main/java/com/breakinblocks/nexus/proxy/ClientProxy.java
@@ -1,19 +1,27 @@
 package com.breakinblocks.nexus.proxy;
 
+import com.breakinblocks.nexus.Nexus;
 import com.breakinblocks.nexus.handlers.TextureHandler;
+import com.breakinblocks.nexus.registry.ModItems;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy {
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
 		super.preInit(event);
+	}
 
+	@Override
+	public void init(FMLInitializationEvent event) {
+		super.init(event);
 		TextureHandler.registerFluidRenderers();
+		ModItems.itemBuffer.forEach(e -> tryHandleItemModel(e,e.getRegistryName().getResourcePath()));
 	}
 
 	@Override
 	public void tryHandleItemModel(Item item, String name) {
-		TextureHandler.handle(item,name);
+		TextureHandler.handle(item, name);
 	}
 }
--- src/main/java/com/breakinblocks/nexus/registry/ModItems.java
+++ src/main/java/com/breakinblocks/nexus/registry/ModItems.java
@@ -6,9 +6,13 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
+import java.util.ArrayList;
+
 public class ModItems {
 	public static Item TABLET;
 	public static ItemStack BUCKETMANAGREEN;
+
+	public static ArrayList<Item> itemBuffer = new ArrayList<>();
 
 	public static void init() {
 		TABLET = register(new ItemTablet(),"tablet");
@@ -19,7 +23,7 @@ public class ModItems {
 		item.setUnlocalizedName(name);
 		item.setCreativeTab(Nexus.tab);
 		GameRegistry.register(item);
-		Nexus.proxy.tryHandleItemModel(item,name);
+		itemBuffer.add(item);
 		return item;
 	}
 }
