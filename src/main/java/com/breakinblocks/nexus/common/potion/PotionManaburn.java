922b87be704f0513e8e923b6b6899d0ec185c442
TeamDman <TeamDman9201@gmail.com>
2017-01-28 23:11:40 -0500

    Add burn damage source

--- src/main/java/com/breakinblocks/nexus/common/potion/PotionManaburn.java
+++ src/main/java/com/breakinblocks/nexus/common/potion/PotionManaburn.java
@@ -1,15 +1,24 @@
 package com.breakinblocks.nexus.common.potion;
 
+import com.breakinblocks.nexus.Nexus;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
-import org.lwjgl.util.*;
-import org.lwjgl.util.Color;
-
-import java.awt.*;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class PotionManaburn extends Potion {
 	public static final int HARMAFTER=3600;
+
+	@Override
+	public boolean hasStatusIcon() {
+		return super.hasStatusIcon();
+	}
+
+	public static final DamageSource burn = new DamageSource(Nexus.MODID+".manaburn").setDamageAllowedInCreativeMode().setDamageBypassesArmor().setDamageIsAbsolute();
 
 	public PotionManaburn() {
 		super(true, 0xFFFFFF);
@@ -25,4 +34,13 @@ public class PotionManaburn extends Potion {
 		System.out.println("boi");
 		entityLivingBaseIn.attackEntityFrom(DamageSource.outOfWorld,2);
 	}
+
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public int getStatusIconIndex() {
+		Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation(Nexus.MODID, "textures/misc/potions.png"));
+		return 0;
+	}
+
 }
